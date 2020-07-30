c8113e86684bb2104450e20bd55fa1b6e3acb535
esac <esac-io@tutanota.com>
2020-07-29 22:44:23 -0300

    Replace delete-file for rename-file

--- src/tangle.el
+++ src/tangle.el
@@ -1,19 +1,15 @@
-;; We can't tangle without org!
+;; we can't tangle without org
 (require 'org)
 
-;; read eos file
+;; read eos org file
 (find-file (expand-file-name "eos.org" user-emacs-directory))
 
-;; tangle it
-(org-babel-tangle)
+;; tangle it (only emacs-lisp source code)
+(org-babel-tangle nil nil 'emacs-lisp)
 
-;; copy to init.el
-(copy-file
+;; rename: eos to init
+(rename-file
   (expand-file-name "eos.el" user-emacs-directory)
   (expand-file-name "init.el" user-emacs-directory)
-  ;; OK-IF-ALREADY-EXISTS, KEEP-TIME,
-  ;; PRESERVE-UID-GID, PRESERVE-PERMISSIONS
-  t t t t)
-
-;; delete eos.el
-(delete-file "eos.el")
+  ;; OK-IF-ALREADY-EXISTS
+  t)

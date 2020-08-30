9534c4e87f78c231deb38352ee51fe6ffa7ac1c3
esac <esac-io@tutanota.com>
2020-08-30 11:59:24 -0300

    Replace eos for vlm

--- src/tangle.el
+++ src/tangle.el
@@ -2,14 +2,14 @@
 (require 'org)
 
 ;; read eos org file
-(find-file (expand-file-name "eos.org" user-emacs-directory))
+(find-file (expand-file-name "vlm.org" user-emacs-directory))
 
 ;; tangle it (only emacs-lisp source code)
 (org-babel-tangle nil nil 'emacs-lisp)
 
 ;; rename: eos to init
 (rename-file
-  (expand-file-name "eos.el" user-emacs-directory)
+  (expand-file-name "vlm.el" user-emacs-directory)
   (expand-file-name "init.el" user-emacs-directory)
   ;; OK-IF-ALREADY-EXISTS
   t)

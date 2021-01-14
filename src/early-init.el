913eb79bf75141fd7fee0e13a671b5a9a89855ab
Isaac <lambdart@protonmail.com>
2021-01-14 14:44:00 -0200

    Remove unnecessary frame clean up, set load-prefer-newer

--- src/early-init.el
+++ src/early-init.el
@@ -1,28 +1,18 @@
 ;;; early-init.el -*- lexical-binding: t; -*-
 
-;; Emacs HEAD (27+) introduces early-init.el, which is run before init.el,
-;; before package and UI initialization happens.
+;; Emacs 27.1 introduced early-init.el, which is run before init.el, before
+;; package and UI initialization happens, and before site files are loaded.
 
-;; Defer garbage collection further back in the startup process
+;; A big contributor to startup times is garbage collection. We up the gc
+;; threshold to temporarily prevent it from running, then reset it later by
+;; enabling `gcmh-mode'. Not resetting it will cause stuttering/freezes.
 (setq gc-cons-threshold most-positive-fixnum)
 
-;; In Emacs 27+, package initialization occurs before `user-init-file' is
-;; loaded, but after `early-init-file'. So,
-;; we must prevent Emacs from doing it early!
+;; In noninteractive sessions, prioritize non-byte-compiled source files to
+;; prevent the use of stale byte-code. Otherwise, it saves us a little IO time
+;; to skip the mtime checks on every *.elc file.
+(setq load-prefer-newer noninteractive)
+
+;; In Emacs 27+, package initialization occurs before
+;; `user-init-file' is loaded, but after `early-init-file'.
 (setq package-enable-at-startup nil)
-(advice-add #'package--ensure-init-file :override #'ignore)
-
-;; Prevent the glimpse of un-styled Emacs by disabling these UI elements early.
-(push '(menu-bar-lines . 0) default-frame-alist)
-(push '(tool-bar-lines . 0) default-frame-alist)
-(push '(vertical-scroll-bars) default-frame-alist)
-
-;; Resizing the Emacs frame can be a terribly expensive part of changing the
-;; font. By inhibiting this, we easily halve startup times with fonts that are
-;; larger than the system default.
-(setq frame-inhibit-implied-resize t)
-
-;; Ignore X resources; its settings would be redundant with the other settings
-;; in this file and can conflict with later config (particularly where the
-;; cursor color is concerned).
-(advice-add #'x-apply-session-resources :override #'ignore)

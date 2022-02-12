15fb0fc257631ba055569ceff3caa6df60b322b8
Isaac <lambdart@protonmail.com>
2022-02-12 11:03:35 -0300

    Simplify the tangle process

--- src/tangle.el
+++ src/tangle.el
@@ -1,25 +1,54 @@
+;;; tangle.el --- summary -*- lexical-binding: t -*-
+;;
+;; Author: lambdart <lambdart@protonmail.com>
+;; Maintainer: lambdart
+;; Homepage: https://github.com/lambdart/tangle.el
+;; Version: 0.0.1 Alpha
+;; Keywords:
+;;
+;; This file is NOT part of GNU Emacs.
+;;
+;;; MIT License
+;;
+;; Copyright (c) 2020,2021,2022 lambdart
+;;
+;; Permission is hereby granted, free of charge, to any person obtaining a copy
+;; of this software and associated documentation files (the "Software"), to deal
+;; in the Software without restriction, including without limitation the rights
+;; to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+;; copies of the Software, and to permit persons to whom the Software is
+;; furnished to do so, subject to the following conditions:
+;;
+;; The above copyright notice and this permission notice shall be included in
+;; all copies or substantial portions of the Software.
+;;
+;; THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+;; IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+;; FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+;; AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+;; LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+;; OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+;; SOFTWARE.
+;;
+;;; Commentary:
+;;
+;;; Code:
+
 ;; tangle the source code blocks require org
 (require 'org)
 
-(defvar vlm-org-file
+(defvar init-org-file
   (expand-file-name "vlm.org" user-emacs-directory)
-  "The vlm.org file - full path.")
+  "The init org file - full path.")
 
-(defvar vlm-tmp-init
-  (expand-file-name "vlm.el" user-emacs-directory)
-  "Temporary file.")
-
-(defvar vlm-new-init
-  (expand-file-name "init.el" user-emacs-directory)
-  "Init file.")
-
-(defun vlm-tangle-and-rename ()
-  "Tangle and rename `vlm-org-file' file."
+(defun tangle-and-rename ()
+  "Tangle and rename `init-org-file'."
   ;; extract elisp source code blocks from vlm.org
-  (org-babel-tangle-file vlm-org-file nil "emacs-lisp")
-  ;; rename files if temporary file exists
-  (when (file-exists-p vlm-tmp-init)
-    (rename-file vlm-tmp-init vlm-new-init t)))
+  ;; return the name of the tangled file
+  (let ((temp (car (org-babel-tangle-file init-org-file nil "emacs-lisp"))))
+    (and (file-exists-p temp)
+         (rename-file temp "init.el" t))))
 
-;; invoke vlm-tangle-and-rename function
-(funcall 'vlm-tangle-and-rename)
+(provide 'tangle)
+
+;;; tangle.el ends here

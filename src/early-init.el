3403c28383705739976d6976e2976751617d3b35
Isaac <lambdart@protonmail.com>
2022-02-12 11:04:32 -0300

    Update tangle target

--- src/early-init.el
+++ src/early-init.el
@@ -3,12 +3,12 @@
 ;; Emacs 27.1 introduced early-init.el, which is run before init.el, before
 ;; package and UI initialization happens, and before site files are loaded.
 
-;; A big contributor to startup times is garbage collection. We up the gc
+;; a big contributor to startup times is garbage collection. We up the gc
 ;; threshold to temporarily prevent it from running, then reset it later by
 ;; enabling `gcmh-mode'. Not resetting it will cause stuttering/freezes.
 (setq gc-cons-threshold most-positive-fixnum)
 
-;; In noninteractive sessions, prioritize non-byte-compiled source files to
+;; in noninteractive sessions, prioritize non-byte-compiled source files to
 ;; prevent the use of stale byte-code. Otherwise, it saves us a little IO time
 ;; to skip the mtime checks on every *.elc file.
 (setq load-prefer-newer noninteractive)
